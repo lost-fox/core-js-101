157b8d33d7482e177ebe9bd3856d5e474daf8beb
lost-fox <Oljaburak@mail.ru>
2022-01-11 23:03:16 +0300

    feat: add solution 4 tasks

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -23,8 +23,8 @@
  *   getComposition(Math.sin, Math.asin)(x) => Math.sin(Math.asin(x))
  *
  */
-function getComposition(/* f, g */) {
-  throw new Error('Not implemented');
+function getComposition(f, g) {
+  return function carry(x) { return f(g(x)); };
 }
 
 
@@ -44,8 +44,8 @@ function getComposition(/* f, g */) {
  *   power05(16) => 4
  *
  */
-function getPowerFunction(/* exponent */) {
-  throw new Error('Not implemented');
+function getPowerFunction(exponent) {
+  return function carry(x) { return x ** exponent; };
 }
 
 
@@ -81,8 +81,9 @@ function getPolynom() {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  const catched = func();
+  return function carry() { return catched; };
 }
 
 
@@ -101,8 +102,20 @@ function memoize(/* func */) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  let num = 0;
+
+  return () => {
+    while (num <= attempts) {
+      try {
+        return func();
+      } catch (err) {
+        num += 0;
+      }
+      num += 1;
+    }
+    return num - 1;
+  };
 }
 
 
